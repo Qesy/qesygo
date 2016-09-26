5786a242161829d3d581be0e181297fcca6e8a19
qesy@163.com <qesy@163.com>
2016-09-26 15:06:34 +0800

    add randstr

--- common.go
+++ common.go
@@ -214,3 +214,18 @@ func Unset(arr []string, str string) []string {
 	}
 	return newArr
 }
+
+//-- kind 0:纯数字，1：小写，2：大写，3：数字+大小写字幕 --
+func Krand(size int, kind int) []byte {
+	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
+	is_all := kind > 2 || kind < 0
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		if is_all { // random ikind
+			ikind = rand.Intn(3)
+		}
+		scope, base := kinds[ikind][0], kinds[ikind][1]
+		result[i] = uint8(base + rand.Intn(scope))
+	}
+	return result
+}
