adc595d736f2e1e3be2b52a30f05417279a0649b
qesy@163.com <qesy@163.com>
2016-10-09 17:44:16 +0800

    update array

--- array.go
+++ array.go
@@ -1,5 +1,10 @@
 package QesyGo
 
+import (
+	"math/rand"
+	"strconv"
+)
+
 func Array_merge(arr ...[]string) []string {
 	var retArr []string
 	for _, n := range arr {
@@ -8,4 +13,40 @@ func Array_merge(arr ...[]string) []string {
 		}
 	}
 	return retArr
+}
+
+func InArray(Arr interface{}, str string) bool {
+	strArr, ok := Arr.([]string)
+	if ok {
+		for _, v := range strArr {
+			if v == str {
+				return true
+			}
+		}
+		return false
+	}
+	mapArr, ok := Arr.(map[string]string)
+	if ok {
+		for _, v := range mapArr {
+			if v == str {
+				return true
+			}
+		}
+		return false
+	}
+	intArr, ok := Arr.([]int)
+	if ok {
+		for _, v := range intArr {
+			if strconv.Itoa(v) == str {
+				return true
+			}
+		}
+		return false
+	}
+	return false
+
+}
+
+func Array_Rand(num int) []int {
+	return rand.Perm(num)
 }
--- common.go
+++ common.go
@@ -71,38 +71,6 @@ func (arr *RandWeihtArr) RandWeight() string {
 	return ret
 }
 
-func InArray(Arr interface{}, str string) bool {
-	strArr, ok := Arr.([]string)
-	if ok {
-		for _, v := range strArr {
-			if v == str {
-				return true
-			}
-		}
-		return false
-	}
-	mapArr, ok := Arr.(map[string]string)
-	if ok {
-		for _, v := range mapArr {
-			if v == str {
-				return true
-			}
-		}
-		return false
-	}
-	intArr, ok := Arr.([]int)
-	if ok {
-		for _, v := range intArr {
-			if strconv.Itoa(v) == str {
-				return true
-			}
-		}
-		return false
-	}
-	return false
-
-}
-
 func ReadFile(str string) ([]byte, error) {
 	return ioutil.ReadFile(str)
 }
