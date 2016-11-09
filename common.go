42123b2b6ae82d40c52cc6bd9b52d53df7a5632b
qesy@163.com <qesy@163.com>
2016-11-09 16:25:08 +0800

    add

--- common.go
+++ common.go
@@ -201,7 +201,6 @@ func Unset(arr []string, str string) []string {
 func Krand(size int, kind int) []byte {
 	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
 	is_all := kind > 2 || kind < 0
-	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < size; i++ {
 		if is_all { // random ikind
 			ikind = Rand(1, 3)
