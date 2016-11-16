970aaefd15edd1b353d2cd44cdc4b6dd3ac47e82
qesy@163.com <qesy@163.com>
2016-11-16 15:58:36 +0800

    update log

--- base.go
+++ base.go
@@ -7,7 +7,7 @@ import (
 
 func Log(str string, logLev string) {
 	FileName := Date(Time("Second"), "20060102")
-	logfile, err := os.OpenFile(logLev+"_"+FileName+".log", os.O_RDWR|os.O_CREATE, 0666)
+	logfile, err := os.OpenFile(logLev+"_"+FileName+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		os.Exit(-1)
 	}
