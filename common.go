dc74a6e730e11b6847fdbc6cf700bedde597500e
qesy@163.com <qesy@163.com>
2017-01-12 14:17:58 +0800

    add md5

--- common.go
+++ common.go
@@ -1,7 +1,9 @@
 package QesyGo
 
 import (
+	"crypto/md5"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -242,4 +244,10 @@ func Base64Decode(json string) ([]byte, error) {
 func Abs(a int) (ret int) {
 	ret = (a ^ a>>31) - a>>31
 	return
+}
+
+func Md5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
