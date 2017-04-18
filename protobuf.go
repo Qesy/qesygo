9b3fac55f39765b7a9ab8d7cbe97635d2ab894ff
qesy@163.com <qesy@163.com>
2017-04-18 10:14:52 +0800

    add protobuf

---
+++ protobuf.go
@@ -0,0 +1,21 @@
+package QesyGo
+
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+func Unmarshal(data []byte, pbStruct proto.Message) error {
+	return proto.Unmarshal(data[4:], pbStruct)
+}
+
+func Marshal(ProtoId int32, pbStruct proto.Message) ([]byte, error) {
+	data := []byte{}
+	if msg, err := proto.Marshal(pbStruct); err != nil {
+		return msg, err
+	} else {
+		PidByte := IntToBytes(ProtoId)
+		data = append(data, PidByte...)
+		data = append(data, msg...)
+	}
+	return data, nil
+}
