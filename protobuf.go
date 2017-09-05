5442d81b3595b00c8aa3c085d5b28044134c709a
qesy@163.com <qesy@163.com>
2017-09-05 15:18:05 +0800

    fix bugs

--- protobuf.go
+++ protobuf.go
@@ -9,13 +9,5 @@ func Unmarshal(data []byte, pbStruct proto.Message) error {
 }
 
 func Marshal(ProtoId int32, pbStruct proto.Message) ([]byte, error) {
-	data := []byte{}
-	if msg, err := proto.Marshal(pbStruct); err != nil {
-		return msg, err
-	} else {
-		PidByte := IntToBytes(ProtoId)
-		data = append(data, PidByte...)
-		data = append(data, msg...)
-	}
-	return data, nil
+	return proto.Marshal(pbStruct)
 }
