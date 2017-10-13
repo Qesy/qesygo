82f8fd92b77a39556f8089f52fff7ac75e25ceb6
qesy@163.com <qesy@163.com>
2017-10-13 13:23:57 +0800

    fix redis bugs

--- redis.go
+++ redis.go
@@ -102,7 +102,7 @@ func (cr *CacheRedis) HGetAll(key string) (map[string]string, error) {
 	return rs, err
 }
 
-func (cr *CacheRedis) ZAdd(Key string, Score int64, Name string) (interface{}, error) {
+func (cr *CacheRedis) ZAdd(Key string, Score int32, Name string) (interface{}, error) {
 	return cr.do("ZADD", Key, Score, Name)
 }
 
