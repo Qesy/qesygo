617545e19a9f497172760d7a2ffff73d345a4ab3
Qesy <762264@qq.com>
2021-10-15 15:41:42 +0800

    add

--- array.go
+++ array.go
@@ -6,6 +6,35 @@ import (
 	"time"
 )
 
+/*
+people := []Person{
+        {"Bob", 31},
+        {"John", 42},
+        {"Michael", 17},
+        {"Jenny", 26},
+    }
+
+    fmt.Println(people)
+    // There are two ways to sort a slice. First, one can define
+    // a set of methods for the slice type, as with ByAge, and
+    // call sort.Sort. In this first example we use that technique.
+    sort.Sort(ByAge(people))
+    fmt.Println(people)
+*/
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+// ByAge implements sort.Interface for []Person based on
+// the Age field.
+type ByAge []Person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
 func Array_merge(arr ...[]string) []string {
 	var retArr []string
 	for _, n := range arr {
