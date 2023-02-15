ad0ddb4439f6a6b704b59e18035b6ed7ef3a73f4
Qesy <762264@qq.com>
2023-02-15 18:02:27 +0800

    add

--- crypto.go
+++ crypto.go
@@ -11,10 +11,10 @@ import (
 	"encoding/pem"
 	"log"
 
-	"github.com/thinkoner/openssl"
+	"github.com/forgoer/openssl"
 )
 
-//16进制解码
+// 16进制解码
 func HexDecode(s string) []byte {
 	dst := make([]byte, hex.DecodedLen(len(s))) //申请一个切片, 指明大小. 必须使用hex.DecodedLen
 	n, err := hex.Decode(dst, []byte(s))        //进制转换, src->dst
@@ -25,7 +25,7 @@ func HexDecode(s string) []byte {
 	return dst[:n] //返回0:n的数据.
 }
 
-//字符串转为16进制
+// 字符串转为16进制
 func HexEncode(s string) []byte {
 	dst := make([]byte, hex.EncodedLen(len(s))) //申请一个切片, 指明大小. 必须使用hex.EncodedLen
 	n := hex.Encode(dst, []byte(s))             //字节流转化成16进制
