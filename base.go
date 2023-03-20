9a630896d537dde44cc648bc059663f6cc0019bb
Qesy <762264@qq.com>
2023-03-20 11:16:50 +0800

    1 改进日志方法

--- base.go
+++ base.go
@@ -3,17 +3,16 @@ package qesygo
 import (
 	"log"
 	"os"
-	"runtime/debug"
 )
 
-func Log(str string, logLev string) {
-	FileName := Date(Time("Second"), "20060102")
-	logfile, err := os.OpenFile(logLev+"_"+FileName+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+func Log(str string) {
+	FileName := "./static/log/log_" + Date(0, "20060102") + ".txt"
+	logfile, err := os.OpenFile(FileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		os.Exit(-1)
+		return
 	}
 	defer logfile.Close()
-	logger := log.New(logfile, "\r\n", log.Ldate|log.Ltime|log.Llongfile)
+	logger := log.New(logfile, "logger", log.Ldate|log.Ltime|log.Llongfile)
 	logger.Println(str)
-	debug.PrintStack()
+
 }
