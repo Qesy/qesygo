86c0365e9ca193c27cfaf2e3f5e19cbfcb3a8060
Qesy <762264@qq.com>
2023-04-06 10:30:11 +0800

    1 日志系统调整

--- base.go
+++ base.go
@@ -1,18 +1,17 @@
 package qesygo
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
-func Log(str string, logLev string) {
-	FileName := "./static/log/" + logLev + "_" + Date(0, "20060102") + ".txt"
-	logfile, err := os.OpenFile(FileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+func Log() {
+	logFile, err := os.OpenFile("./static/log/error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return
 	}
-	defer logfile.Close()
-	logger := log.New(logfile, "logger", log.Ldate|log.Ltime|log.Llongfile)
-	logger.Println(str)
-
+	log.SetFlags(log.Llongfile | log.Ltime | log.Ldate)
+	log.SetOutput(logFile)
+	fmt.Println("Log Start Success !")
 }
