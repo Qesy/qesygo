661e78cf3cf20597f695f4569c09d62a03912a0e
Qesy <762264@qq.com>
2023-04-17 19:06:48 +0800

    1 雪花算法还原

--- SnowFlake.go
+++ SnowFlake.go
@@ -2,68 +2,143 @@ package qesygo
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 	"time"
 )
 
-const (
-	workerBits  uint8 = 10                      // 节点数
-	seqBits     uint8 = 12                      // 1毫秒内可生成的id序号的二进制位数
-	workerMax   int64 = -1 ^ (-1 << workerBits) // 节点ID的最大值，用于防止溢出
-	seqMax      int64 = -1 ^ (-1 << seqBits)    // 同上，用来表示生成id序号的最大值
-	timeShift   uint8 = workerBits + seqBits    // 时间戳向左的偏移量
-	workerShift uint8 = seqBits                 // 节点ID向左的偏移量
-	epoch       int64 = 1567906170596           // 开始运行时间
-)
+type SnowFlakeIdWorker struct {
 
-type Worker struct {
-	// 添加互斥锁 确保并发安全
-	mu sync.Mutex
-	// 记录时间戳
-	timestamp int64
-	// 该节点的ID
+	// 开始时间戳
+	twepoch int64
+
+	// 机器ID所占的位数
+	workerIdBits int64
+
+	// 数据标识ID所占的位数
+	dataCenterIdBits int64
+
+	// 支持的最大机器ID
+	maxWorkerId int64
+
+	// 支持的最大机房 ID
+	maxDataCenterId int64
+
+	// 序列在ID中占的位数
+	sequenceBits int64
+
+	// 机器ID向左移位数
+	workerIdShift int64
+
+	// 机房ID向左移位数
+	dataCenterIdShift int64
+
+	// 时间截向左移位数
+	timestampLeftShift int64
+
+	// 生成序列的掩码最大值
+	sequenceMask int64
+
+	// 工作机器ID
 	workerId int64
-	// 当前毫秒已经生成的id序列号(从0开始累加) 1毫秒内最多生成4096个ID
-	seq int64
+
+	// 机房ID
+	dataCenterId int64
+
+	/**
+	 * 毫秒内序列
+	 */
+	sequence int64
+
+	// 上次生成ID的时间戳
+	lastTimestamp int64
+
+	// 锁
+	lock sync.Mutex
 }
 
-// 实例化对象
-func NewWorker(workerId int64) (*Worker, error) {
-	// 要先检测workerId是否在上面定义的范围内
-	if workerId < 0 || workerId > workerMax {
-		return nil, errors.New("Worker ID excess of quantity")
+func CreatIdMultiple(dataCenterId int64, workerId int64) int64 { //分布式
+	idWorker := SnowFlakeIdWorker{}
+	idWorker.Init(dataCenterId, workerId)
+	return idWorker.NextId()
+}
+func (p *SnowFlakeIdWorker) Init(dataCenterId int64, workerId int64) {
+	// 开始时间戳；这里是2021-06-01
+	p.twepoch = 1622476800000
+	// 机器ID所占的位数
+	p.workerIdBits = 5
+	// 数据标识ID所占的位数
+	p.dataCenterIdBits = 5
+	// 支持的最大机器ID，最大是31
+	p.maxWorkerId = -1 ^ (-1 << p.workerIdBits)
+	// 支持的最大机房ID，最大是 31
+	p.maxDataCenterId = -1 ^ (-1 << p.dataCenterIdBits)
+	// 序列在ID中占的位数
+	p.sequenceBits = 12
+	// 机器ID向左移12位
+	p.workerIdShift = p.sequenceBits
+	// 机房ID向左移17位
+	p.dataCenterIdShift = p.sequenceBits + p.workerIdBits
+	// 时间截向左移22位
+	p.timestampLeftShift = p.sequenceBits + p.workerIdBits + p.dataCenterIdBits
+	// 生成序列的掩码最大值，最大为4095
+	p.sequenceMask = -1 ^ (-1 << p.sequenceBits)
+
+	if workerId > p.maxWorkerId || workerId < 0 {
+		panic(errors.New(fmt.Sprintf("Worker ID can't be greater than %d or less than 0", p.maxWorkerId)))
 	}
-	// 生成一个新节点
-	return &Worker{
-		timestamp: 0,
-		workerId:  workerId,
-		seq:       0,
-	}, nil
+	if dataCenterId > p.maxDataCenterId || dataCenterId < 0 {
+		panic(errors.New(fmt.Sprintf("DataCenter ID can't be greater than %d or less than 0", p.maxDataCenterId)))
+	}
+
+	p.workerId = workerId
+	p.dataCenterId = dataCenterId
+	// 毫秒内序列(0~4095)
+	p.sequence = 0
+	// 上次生成 ID 的时间戳
+	p.lastTimestamp = -1
 }
 
-// 获取一个新ID
-func (w *Worker) Next() int64 {
-	// 获取id最关键的一点 加锁 加锁 加锁
-	w.mu.Lock()
-	defer w.mu.Unlock() // 生成完成后记得 解锁 解锁 解锁
-	// 获取生成时的时间戳
-	now := time.Now().UnixNano() / 1e6 // 纳秒转毫秒
-	if w.timestamp == now {
-		w.seq = (w.seq + 1) & seqMax
-		// 这里要判断，当前工作节点是否在1毫秒内已经生成seqMax个ID
-		if w.seq == 0 {
-			// 如果当前工作节点在1毫秒内生成的ID已经超过上限 需要等待1毫秒再继续生成
-			for now <= w.timestamp {
-				now = time.Now().UnixNano() / 1e6
-			}
+// 生成ID，注意此方法已经通过加锁来保证线程安全
+func (p *SnowFlakeIdWorker) NextId() int64 {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	timestamp := p.timeGen()
+	// 如果当前时间小于上一次 ID 生成的时间戳，说明发生时钟回拨，为保证ID不重复抛出异常。
+	if timestamp < p.lastTimestamp {
+		panic(errors.New(fmt.Sprintf("Clock moved backwards. Refusing to generate id for %d milliseconds", p.lastTimestamp-timestamp)))
+	}
+
+	if p.lastTimestamp == timestamp {
+		// 同一时间生成的，则序号+1
+		p.sequence = (p.sequence + 1) & p.sequenceMask
+		// 毫秒内序列溢出：超过最大值
+		if p.sequence == 0 {
+			// 阻塞到下一个毫秒，获得新的时间戳
+			timestamp = p.tilNextMillis(p.lastTimestamp)
 		}
 	} else {
-		// 如果当前时间与工作节点上一次生成ID的时间不一致 则需要重置工作节点生成ID的序号
-		w.seq = 0
+		// 时间戳改变，序列重置
+		p.sequence = 0
 	}
-	w.timestamp = now // 将机器上一次生成ID的时间更新为当前时间
-	// 第一段 now - epoch 为该算法目前已经奔跑了xxx毫秒
-	// 如果在程序跑了一段时间修改了epoch这个值 可能会导致生成相同的ID
-	ID := int64((now-epoch)<<timeShift | (w.workerId << workerShift) | (w.seq))
-	return ID
+	// 保存本次的时间戳
+	p.lastTimestamp = timestamp
+
+	// 移位并通过或运算拼到一起
+	return ((timestamp - p.twepoch) << p.timestampLeftShift) |
+		(p.dataCenterId << p.dataCenterIdShift) |
+		(p.workerId << p.workerIdShift) | p.sequence
+}
+
+func (p *SnowFlakeIdWorker) tilNextMillis(lastTimestamp int64) int64 {
+	timestamp := p.timeGen()
+	for timestamp <= lastTimestamp {
+		timestamp = p.timeGen()
+	}
+	return timestamp
+}
+
+func (p *SnowFlakeIdWorker) timeGen() int64 {
+	return time.Now().UnixNano() / 1e6
 }
