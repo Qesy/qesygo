3dd27f91c8318bccc316cfa867df2fafbd1a7094
Qesy <762264@qq.com>
2023-05-19 14:25:18 +0800

    1 改回 map[string]string

--- array.go
+++ array.go
@@ -170,7 +170,7 @@ func Array_unique(Arr []string) []string {
 	return NewArr
 }
 
-func Keys(Arr map[string]interface{}) []string {
+func Keys(Arr map[string]string) []string {
 	Keys := []string{}
 	for k, _ := range Arr {
 		Keys = append(Keys, k)
