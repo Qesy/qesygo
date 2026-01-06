fa3dae016cd6df223c023327874c157664ffe9c0
Qesy <762264@qq.com>
2026-01-06 15:01:42 +0800

    1 增加密码复杂度验证

--- check.go
+++ check.go
@@ -1,6 +1,9 @@
 package qesygo
 
-import "regexp"
+import (
+	"regexp"
+	"unicode"
+)
 
 func CheckMobile(phone string) bool { // CheckMobile 检验手机号 (第一位必为1的十一位数字)
 	regRuler := "^1\\d{10}$"
@@ -17,4 +20,28 @@ func CheckIdCard(card string) bool { // CheckIdCard 检验身份证
 	regRuler := "(^\\d{15}$)|(^\\d{18}$)|(^\\d{17}(\\d|X|x)$)"
 	reg := regexp.MustCompile(regRuler)
 	return reg.MatchString(card)
+}
+
+// CheckPasswordComplexity 校验密码复杂度
+// 规则：
+// 1. 长度不少于 8 位
+// 2. 必须包含：大写字母、小写字母、数字、特殊字符
+func CheckPasswordComplexity(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+	var hasUpper, hasLower, hasDigit, hasSpecial bool
+	for _, r := range password {
+		switch {
+		case unicode.IsUpper(r):
+			hasUpper = true
+		case unicode.IsLower(r):
+			hasLower = true
+		case unicode.IsDigit(r):
+			hasDigit = true
+		case unicode.IsPunct(r) || unicode.IsSymbol(r):
+			hasSpecial = true
+		}
+	}
+	return hasUpper && hasLower && hasDigit && hasSpecial
 }
